d7f1ceaa9494066bfbbc758241bb274d4c6d0aaf
Yash <kansagara.yash27@gmail.com>
2023-02-12 22:11:21 -0500

    yash's commit

--- src/main/java/com/practice/jan2023/App.java
+++ src/main/java/com/practice/jan2023/App.java
@@ -20,11 +20,11 @@ class B1 extends A {
 	public static void main(String args[]) {
 		 String s1="Sachin";  
 		   String s2="Sachin";  
-		   String s3="Ratan";  
+		   String s4="Ratan";  
 		   if(s1.compareTo(s2) != 0 )
 		   System.out.println(s1.compareTo(s2));//0  
 		   System.out.println(s1.compareTo(s3));//1(because s1>s3)  
-		   System.out.println(s3.compareTo(s1));//-1(because s3 < s1 )  
+		   System.out.println(s4.compareTo(s1));//-1(because s3 < s1 )  
 
 
 		
